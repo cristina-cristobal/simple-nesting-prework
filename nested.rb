f28209413a24a75bf57c4206cbfc023b6cf5ba8c
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-29 20:22:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
+
 		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 		 programmer_hash
 
@@ -129,5 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
